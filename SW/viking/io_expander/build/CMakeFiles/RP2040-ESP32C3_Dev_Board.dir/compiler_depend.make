6eb72ab9be0c358f8983e53d1d273932822dcab1
erdemkara35 <erdem.kara@hotmail.com>
2023-05-18 00:15:54 +0300

    io_expander project fix

--- SW/viking/io_expander/build/CMakeFiles/RP2040-ESP32C3_Dev_Board.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for RP2040-ESP32C3_Dev_Board.
-# This may be replaced when dependencies are built.
--- SW/viking/io_expander/build/CMakeFiles/RP2040-ESP32C3_Dev_Board.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for RP2040-ESP32C3_Dev_Board.
