8182b81507071e7c64c8ee7cc54aec60060db7ea
Caner Alp <caner.alp@alpelectronix.com>
2023-07-30 18:35:41 +0200

    Control the Buttons with Interrupt

--- SW/viking/button/main.c
+++ SW/viking/button/main.c
@@ -5,6 +5,24 @@
 #define BUTTON_UP_PIN 20
 #define BUTTON_DOWN_PIN 19
 
+// Interrupt Callback function
+void gpio_callback(uint gpio, uint32_t events) {
+
+
+    // if(gpio_get(VIKING_LED_PIN) == 1)
+    // gpio_put(VIKING_LED_PIN, 0);
+    // else 
+    // gpio_put(VIKING_LED_PIN, 1);
+
+    // if(gpio == BUTTON_UP_PIN){
+    //     gpio_put(VIKING_LED_PIN, 0);}
+    // if(gpio == BUTTON_DOWN_PIN){
+    //     gpio_put(VIKING_LED_PIN, 1);}
+
+    // toggle the LED Pin
+    gpio_xor_mask((1ul << VIKING_LED_PIN));
+}
+
 int main()
 {
     stdio_init_all();
@@ -16,17 +34,22 @@ int main()
 
     gpio_init(VIKING_LED_PIN);
     gpio_set_dir(VIKING_LED_PIN, GPIO_OUT);
-    gpio_put(VIKING_LED_PIN, 1);
+    gpio_put(VIKING_LED_PIN, 0);
+
+    // Set the Interrupt for each pin one by one, doesnt support multi pin at once.
+    gpio_set_irq_enabled_with_callback(BUTTON_UP_PIN ,GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+    gpio_set_irq_enabled_with_callback(BUTTON_DOWN_PIN,GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
 
     while (true)
     {
-        if (gpio_get(BUTTON_UP_PIN) == false)
+/*         if (gpio_get(BUTTON_UP_PIN) == false)
         {
             gpio_put(VIKING_LED_PIN, 1);
         }
         else if (gpio_get(BUTTON_DOWN_PIN) == false)
         {
             gpio_put(VIKING_LED_PIN, 0);
-        }
+        } */
     }
 }
+
